29db75d1ce0a6216ad338e97b80e216e08624836
Josh Perez <josh@goatslacker.com>
2015-01-08 12:28:48 -0800

    Barebones iso
    Moving the react stuff out to lib and making the regular iso just
    barebones so it can support different libraries and patterns.

---
+++ dist/iso.js
@@ -0,0 +1,68 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.iso=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+"use strict";
+
+module.exports = {
+  server: server,
+  client: client
+};
+
+var escapeTextForBrowser = _dereq_("react/lib/escapeTextForBrowser");
+
+function server(html, data, meta) {
+  if (data === undefined) data = {};
+  if (meta === undefined) meta = {};
+  var xState = escapeTextForBrowser(JSON.stringify(data));
+  var xMeta = escapeTextForBrowser(JSON.stringify(meta));
+
+  return ("<div class=\"node-iso-v3\"\n      data-state=\"" + xState + "\"\n      data-meta=\"" + xMeta + "\">\n  " + html + "\n</div>");
+}
+
+function client(cb) {
+  Array.prototype.forEach.call(document.querySelectorAll(".node-iso-v3"), function (node) {
+    return cb(node.dataset.state, node, node.dataset.meta);
+  });
+}
+
+},{"react/lib/escapeTextForBrowser":2}],2:[function(_dereq_,module,exports){
+/**
+ * Copyright 2013-2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule escapeTextForBrowser
+ * @typechecks static-only
+ */
+
+"use strict";
+
+var ESCAPE_LOOKUP = {
+  "&": "&amp;",
+  ">": "&gt;",
+  "<": "&lt;",
+  "\"": "&quot;",
+  "'": "&#x27;"
+};
+
+var ESCAPE_REGEX = /[&><"']/g;
+
+function escaper(match) {
+  return ESCAPE_LOOKUP[match];
+}
+
+/**
+ * Escapes text to prevent scripting attacks.
+ *
+ * @param {*} text Text value to escape.
+ * @return {string} An escaped string.
+ */
+function escapeTextForBrowser(text) {
+  return ('' + text).replace(ESCAPE_REGEX, escaper);
+}
+
+module.exports = escapeTextForBrowser;
+
+},{}]},{},[1])(1)
+});
---
+++ src/iso.js
@@ -0,0 +1,26 @@
+module.exports = {
+  server: server,
+  client: client
+}
+
+var escapeTextForBrowser = require('react/lib/escapeTextForBrowser')
+
+function server(html, data = {}, meta = {}) {
+  var xState = escapeTextForBrowser(JSON.stringify(data))
+  var xMeta = escapeTextForBrowser(JSON.stringify(meta))
+
+  return (
+`<div class="node-iso-v3"
+      data-state="${xState}"
+      data-meta="${xMeta}">
+  ${html}
+</div>`
+  )
+}
+
+function client(cb) {
+  Array.prototype.forEach.call(
+    document.querySelectorAll('.node-iso-v3'),
+    node => cb(node.dataset.state, node, node.dataset.meta)
+  )
+}
