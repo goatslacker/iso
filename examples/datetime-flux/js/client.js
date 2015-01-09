36c9b48afa6d73f5f4a1fbc9af77516bdea43cc9
Josh Perez <josh@goatslacker.com>
2015-01-08 20:36:44 -0800

    package.json handles build file gracefully

---
+++ examples/datetime-flux/js/client.js
@@ -0,0 +1,3 @@
+var iso = require('../../../lib/react')
+var components = require('../components')
+iso.client(components)
--- examples/react-router-flux/js/client.js
+++ examples/react-router-flux/js/client.js
@@ -1,11 +1,11 @@
-var iso = require('../../')
+var iso = require('../../../')
 
 var Router = require('react-router')
 var React = require('react')
 
-var routes = require('./src/routes.jsx')
+var routes = require('../src/routes.jsx')
 
-var alt = require('./src/alt')
+var alt = require('../src/alt')
 
 // Once we bootstrap the stores, we run react-router using
 // Router.HistoryLocation
