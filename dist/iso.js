47f2ec7fa83cbd9fef8fc18c77b03ba025d0ae97
michaelbenin <michaellouisbenin@gmail.com>
2015-06-08 21:53:08 -0400

    Updated babel to 5.5.5

--- dist/iso.js
+++ dist/iso.js
@@ -1,22 +1,26 @@
-"use strict";
+'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var escapeTextForBrowser = require("escape-html");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var each = function (x, f) {
+var escapeTextForBrowser = require('escape-html');
+
+var each = function each(x, f) {
   return Array.prototype.forEach.call(x, f);
 };
-var parse = function (node, x) {
+var parse = function parse(node, x) {
   return JSON.parse(node.getAttribute(x));
 };
-var setDefaults = function (config) {
-  config.markupClassName = config.markupClassName || "___iso-html___";
-  config.markupElement = config.markupElement || "div";
-  config.dataClassName = config.dataClassName || "___iso-state___";
-  config.dataElement = config.dataElement || "div";
+var setDefaults = function setDefaults(config) {
+  config.markupClassName = config.markupClassName || '___iso-html___';
+  config.markupElement = config.markupElement || 'div';
+  config.dataClassName = config.dataClassName || '___iso-state___';
+  config.dataElement = config.dataElement || 'div';
 };
 
 var Iso = (function () {
@@ -32,96 +36,95 @@ var Iso = (function () {
     this.data = [];
   }
 
-  _createClass(Iso, {
-    add: {
-      value: function add(html) {
-        var _state = arguments[1] === undefined ? {} : arguments[1];
+  _createClass(Iso, [{
+    key: 'add',
+    value: function add(html) {
+      var _state = arguments[1] === undefined ? {} : arguments[1];
 
-        var _meta = arguments[2] === undefined ? {} : arguments[2];
+      var _meta = arguments[2] === undefined ? {} : arguments[2];
 
-        var state = escapeTextForBrowser(JSON.stringify(_state));
-        var meta = escapeTextForBrowser(JSON.stringify(_meta));
-        this.html.push(html);
-        this.data.push({ state: state, meta: meta });
-        return this;
-      }
-    },
-    render: {
-      value: function render() {
-        var _this = this;
-
-        var markup = this.html.reduce(function (markup, html, i) {
-          return markup + ("<" + _this.markupElement + " class=\"" + _this.markupClassName + "\" data-key=\"" + i + "\">" + html + "</" + _this.markupElement + ">");
-        }, "");
-
-        var data = this.data.reduce(function (nodes, data, i) {
-          var state = data.state;
-          var meta = data.meta;
-
-          return nodes + ("<" + _this.dataElement + " class=\"" + _this.dataClassName + "\" data-key=\"" + i + "\" data-meta=\"" + meta + "\" data-state=\"" + state + "\"></" + _this.dataElement + ">");
-        }, "");
-
-        return "\n" + markup + "\n" + data + "\n";
-      }
+      var state = escapeTextForBrowser(JSON.stringify(_state));
+      var meta = escapeTextForBrowser(JSON.stringify(_meta));
+      this.html.push(html);
+      this.data.push({ state: state, meta: meta });
+      return this;
     }
   }, {
-    render: {
-      value: function render(html) {
-        var state = arguments[1] === undefined ? {} : arguments[1];
-        var meta = arguments[2] === undefined ? {} : arguments[2];
-        var config = arguments[3] === undefined ? { markupClassName: "___iso-html___", markupElement: "div", dataClassName: "___iso-state___", dataElement: "div" } : arguments[3];
+    key: 'render',
+    value: function render() {
+      var _this = this;
 
-        return new Iso(config).add(html, state, meta).render();
+      var markup = this.html.reduce(function (markup, html, i) {
+        return markup + ('<' + _this.markupElement + ' class="' + _this.markupClassName + '" data-key="' + i + '">' + html + '</' + _this.markupElement + '>');
+      }, '');
+
+      var data = this.data.reduce(function (nodes, data, i) {
+        var state = data.state;
+        var meta = data.meta;
+
+        return nodes + ('<' + _this.dataElement + ' class="' + _this.dataClassName + '" data-key="' + i + '" data-meta="' + meta + '" data-state="' + state + '"></' + _this.dataElement + '>');
+      }, '');
+
+      return '\n' + markup + '\n' + data + '\n';
+    }
+  }], [{
+    key: 'render',
+    value: function render(html) {
+      var state = arguments[1] === undefined ? {} : arguments[1];
+      var meta = arguments[2] === undefined ? {} : arguments[2];
+      var config = arguments[3] === undefined ? { markupClassName: '___iso-html___', markupElement: 'div', dataClassName: '___iso-state___', dataElement: 'div' } : arguments[3];
+
+      return new Iso(config).add(html, state, meta).render();
+    }
+  }, {
+    key: 'bootstrap',
+    value: function bootstrap(onNode) {
+      var markupClassName = arguments[1] === undefined ? '___iso-html___' : arguments[1];
+      var dataClassName = arguments[2] === undefined ? '___iso-state___' : arguments[2];
+
+      if (!onNode) {
+        return;
       }
-    },
-    bootstrap: {
-      value: function bootstrap(onNode) {
-        var markupClassName = arguments[1] === undefined ? "___iso-html___" : arguments[1];
-        var dataClassName = arguments[2] === undefined ? "___iso-state___" : arguments[2];
 
-        if (!onNode) {
+      var nodes = document.querySelectorAll('.' + markupClassName);
+      var state = document.querySelectorAll('.' + dataClassName);
+
+      var cache = {};
+
+      each(state, function (node) {
+        var meta = parse(node, 'data-meta');
+        var state = parse(node, 'data-state');
+        cache[node.getAttribute('data-key')] = { meta: meta, state: state };
+      });
+
+      each(nodes, function (node) {
+        var key = node.getAttribute('data-key');
+        if (!cache[key]) {
           return;
         }
+        var _cache$key = cache[key];
+        var meta = _cache$key.meta;
+        var state = _cache$key.state;
 
-        var nodes = document.querySelectorAll("." + markupClassName);
-        var state = document.querySelectorAll("." + dataClassName);
+        onNode(state, meta, node);
+      });
 
-        var cache = {};
-
-        each(state, function (node) {
-          var meta = parse(node, "data-meta");
-          var state = parse(node, "data-state");
-          cache[node.getAttribute("data-key")] = { meta: meta, state: state };
-        });
-
-        each(nodes, function (node) {
-          var key = node.getAttribute("data-key");
-          if (!cache[key]) {
-            return;
-          }
-          var _cache$key = cache[key];
-          var meta = _cache$key.meta;
-          var state = _cache$key.state;
-
-          onNode(state, meta, node);
-        });
-
-        cache = null;
-      }
-    },
-    on: {
-      value: function on(metaKey, metaValue, onNode) {
-        Iso.bootstrap(function (state, meta, node) {
-          if (meta[metaKey] && meta[metaKey] === metaValue) {
-            onNode(state, meta, node);
-          }
-        });
-      }
+      cache = null;
     }
-  });
+  }, {
+    key: 'on',
+    value: function on(metaKey, metaValue, onNode) {
+      Iso.bootstrap(function (state, meta, node) {
+        if (meta[metaKey] && meta[metaKey] === metaValue) {
+          onNode(state, meta, node);
+        }
+      });
+    }
+  }]);
 
   return Iso;
 })();
 
-module.exports = Iso;
+exports['default'] = Iso;
+module.exports = exports['default'];
 
